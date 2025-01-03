ea817493fc5ddb44e8773b68b4056a56d5e1369b
hiroky1983 <hirockysan1983@gmail.com>
2025-01-03 23:45:06 +0900

    init

---
+++ main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	// .envファイルを読み込む
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+
+	c := &cobra.Command{Use: "api [command]"}
+
+	c.AddCommand(
+		&cobra.Command{Use: "start", Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Starting API")
+
+			file, err := os.Create("example.txt")
+			if err != nil {
+				fmt.Println("Error creating file:", err)
+				return
+			}
+			defer file.Close()
+
+			_, err = file.WriteString("This is a sample text.")
+			if err != nil {
+				fmt.Println("Error writing to file:", err)
+				return
+			}
+
+			fmt.Println("File created successfully")
+		}},
+		&cobra.Command{Use: "create-json", Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Creating empty JSON file")
+
+			file, err := os.Create("api.json")
+			if err != nil {
+				fmt.Println("Error creating JSON file:", err)
+				return
+			}
+			defer file.Close()
+
+			_, err = file.WriteString("{}")
+			if err != nil {
+				fmt.Println("Error writing to JSON file:", err)
+				return
+			}
+
+			fmt.Println("Empty JSON file created successfully")
+		}},
+		&cobra.Command{Use: "get", Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Stopping API")
+
+			databaseID := os.Getenv("NOTION_DATABASE_ID")
+			url := fmt.Sprintf("https://api.notion.com/v1/databases/%s", databaseID)
+
+			req, _ := http.NewRequest("GET", url, nil)
+
+			NOTION_ACCESS_TOKEN := os.Getenv("NOTION_ACCESS_TOKEN")
+
+			req.Header.Add("accept", "application/json")
+			req.Header.Add("Notion-Version", "2022-06-28")
+			req.Header.Add("Authorization", "Bearer "+NOTION_ACCESS_TOKEN)
+
+			res, _ := http.DefaultClient.Do(req)
+
+			defer res.Body.Close()
+			body, _ := io.ReadAll(res.Body)
+
+			fmt.Println(string(body))
+		}},
+		&cobra.Command{Use: "create-page", Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Creating page from api.json")
+
+			file, err := os.Open("api.json")
+			if err != nil {
+				fmt.Println("Error opening JSON file:", err)
+				return
+			}
+			defer file.Close()
+
+			var data map[string]interface{}
+			decoder := json.NewDecoder(file)
+			if err := decoder.Decode(&data); err != nil {
+				fmt.Println("Error decoding JSON:", err)
+				return
+			}
+
+			url := "https://api.notion.com/v1/pages"
+			NOTION_ACCESS_TOKEN := os.Getenv("NOTION_ACCESS_TOKEN")
+
+			jsonData, err := json.Marshal(data)
+			if err != nil {
+				fmt.Println("Error marshalling JSON:", err)
+				return
+			}
+
+			req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+			if err != nil {
+				fmt.Println("Error creating request:", err)
+				return
+			}
+
+			req.Header.Add("Content-Type", "application/json")
+			req.Header.Add("Notion-Version", "2022-06-28")
+			req.Header.Add("Authorization", "Bearer "+NOTION_ACCESS_TOKEN)
+
+			res, err := http.DefaultClient.Do(req)
+			if err != nil {
+				fmt.Println("Error making request:", err)
+				return
+			}
+			defer res.Body.Close()
+
+			body, err := io.ReadAll(res.Body)
+			if err != nil {
+				fmt.Println("Error reading response:", err)
+				return
+			}
+			// 処理が完了したらapi.jsonを空にする
+			file, err = os.Create("api.json")
+			if err != nil {
+				fmt.Println("Error creating JSON file:", err)
+				return
+			}
+			defer file.Close()
+
+			fmt.Println("Response from Notion API:", string(body))
+		}},
+	)
+
+	c.Execute()
+}
